b907e927fe36b3d2183f4125104aee0fc16c5d41
YevheniiYevtushenko <yevheniiyevtushenkov@gmail.com>
2022-09-26 13:22:59 +0300

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        short s1 = 99, s2 = 99;
+        float f1, f2;
+        byte b = -10;
+        boolean q = true;
+        s1 = (short) Math.pow(b, 2);
+        s2 = (short) ((Math.random() * (10 - 0)) + 0);
+        float d1, d2;
+        d2 = (float) ((Math.random() * (9999 - 1)) + 1);
+        d1 = d2/(s1*s2);
+
     }
 }
