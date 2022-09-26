f803d12673a71aa2a21ccb34cab5b63bff48d372
YevheniiYevtushenko <yevheniiyevtushenkov@gmail.com>
2022-09-26 13:37:13 +0300

    Commit 4

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,10 @@ public class Main {
         boolean q = true;
         s1 = (short) Math.pow(b, 2);
         s2 = (short) ((Math.random() * (10 - 0)) + 0);
-        float d1, d2;
-        d2 = (float) ((Math.random() * (9999 - 1)) + 1);
-        d1 = d2/(s1*s2);
+        f2 = (float) ((Math.random() * (9999 - 1)) + 1);
+        f1 = f2/(s1*s2);
         System.out.println("s1 = " + s1 + "\ns2 = " + s2 +
-                "\nd1 = " + d1 + "\nd2 = " + d2);
+                "\nf1 = " + f1 + "\nf2 = " + f2);
+        //System.out.println("y = " + )
     }
 }
